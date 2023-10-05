af85284c5b44c354a3cebf6d44ee26a96527531f
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2023-10-05 12:37:36 -0700

    initial commit
    adding my starter files

---
+++ Cocktail Site/js/main.js
@@ -0,0 +1,20 @@
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+  const drinkEntry = document.querySelector('input').value
+
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkEntry}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks)
+     
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+
+// document.querySelector('h2').innerText = data.drinks[0].strDrink
+// document.querySelector('img').src = data.drinks[0].strDrinkThumb + "/preview"
+// document.querySelector('h3').innerText = data.drinks[0].strInstructions
