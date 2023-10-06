2daf582f7da647a67ff2b926be2db5954036f8a7
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2023-10-06 14:45:51 -0700

    carousel setup

--- Cocktail Site/js/main.js
+++ Cocktail Site/js/main.js
@@ -1,20 +1,79 @@
-document.querySelector('button').addEventListener('click', getDrink)
+document.querySelector('#findDrink').addEventListener('click', getDrink)
 
 function getDrink(){
   const drinkEntry = document.querySelector('input').value
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkEntry}`)
-    .then(res => res.json()) // parse response as JSON
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkEntry}`)
+    .then(res => res.json())
     .then(data => {
-      console.log(data.drinks)
-     
+      console.log(data.drinks);
+      const carousel = document.querySelector('.carousel');
+      carousel.innerHTML = ''; // Clear existing content
+
+      //populate the data in the HTML by creating elements from the event listener
+      data.drinks.forEach((drink, index) => {
+        carousel.innerHTML += `
+          <div class="drinkEntry${index}">
+            <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
+            <h2>${drink.strDrink}</h2>
+          </div>
+        `;
+      });
+
+      // Add the active-carousel class to activate styling that only shows whent he carousel is in use
+      const carouselContainer = document.querySelector('.carousel-container');
+      carouselContainer.classList.add('active-carousel');
+
+      //
+      const prevBtn = document.getElementById('prevBtn');
+      const nextBtn = document.getElementById('nextBtn');
+      let currentIndex = 0;
+
+      // Update button visibility based on currentIndex
+      function updateButtonVisibility() {
+        if (currentIndex === 0) {
+          prevBtn.style.display = 'block'; // Show "Previous" button at the first item
+        } else {
+          prevBtn.style.display = 'block'; // Show "Previous" button for other items
+        }
+      
+        if (currentIndex === carousel.children.length - 1) {
+          nextBtn.style.display = 'none'; // Hide "Next" button at the last item
+        } else {
+          nextBtn.style.display = 'block'; // Show "Next" button for other items
+        }
+      }
+
+      // Initial setup
+      updateButtonVisibility();
+
+      prevBtn.addEventListener('click', () => {
+        if (currentIndex > 0) {
+          currentIndex--;
+        } else {
+          // If at the first item, wrap around to the last item
+          currentIndex = carousel.children.length - 1;
+        }
+        updateCarousel();
+      });
+
+      nextBtn.addEventListener('click', () => {
+        if (currentIndex < carousel.children.length - 1) {
+          currentIndex++;
+        } else {
+          // If at the last item, wrap around to the first item
+          currentIndex = 0;
+        }
+        updateCarousel();
+      });
+
+      function updateCarousel() {
+        const itemWidth = carousel.children[0].offsetWidth;
+        const newPosition = -currentIndex * itemWidth;
+        carousel.style.transform = `translateX(${newPosition}px)`;
+      }
     })
     .catch(err => {
-        console.log(`error ${err}`)
+      console.log(`error ${err}`);
     });
 }
-
-
-// document.querySelector('h2').innerText = data.drinks[0].strDrink
-// document.querySelector('img').src = data.drinks[0].strDrinkThumb + "/preview"
-// document.querySelector('h3').innerText = data.drinks[0].strInstructions
