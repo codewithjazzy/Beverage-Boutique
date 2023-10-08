ad695bf89401a41a7d205a4e7ffff480a782a400
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2023-10-07 23:20:54 -0700

    color pallete

--- Cocktail Site/js/main.js
+++ Cocktail Site/js/main.js
@@ -9,18 +9,27 @@ function getDrink(){
       console.log(data.drinks);
       const carousel = document.querySelector('.carousel');
       carousel.innerHTML = ''; // Clear existing content
+      const cocktailSelected = document.querySelector('.cocktailSelected');
+      cocktailSelected.innerHTML = '';
 
-      //populate the data in the HTML by creating elements from the event listener
-      data.drinks.forEach((drink, index) => {
-        carousel.innerHTML += `
-          <div class="drinkEntry${index}">
-            <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
-            <h2>${drink.strDrink}</h2>
-          </div>
-        `;
-      });
+  //loop that grabs each drinkEntry result, and then puts data in the HTML by creating elements to put each result into
+  data.drinks.forEach((drink, index) => {
+    const drinkEntryDiv = document.createElement('div');
+    drinkEntryDiv.classList.add('drinkEntry'); // Add the class
+    drinkEntryDiv.innerHTML = `
+      <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
+      <h2>${drink.strDrink}</h2>
+    `;
+  
+    // Make each drink in the carousel clickable
+    drinkEntryDiv.addEventListener('click', () => {
+      displayDrinkDetails(drink);
+    });
+  
+    carousel.appendChild(drinkEntryDiv);
+  });
 
-      // Add the active-carousel class to activate styling that only shows whent he carousel is in use
+    // Add the active-carousel class to activate styling that only shows whent the carousel is in use
       const carouselContainer = document.querySelector('.carousel-container');
       carouselContainer.classList.add('active-carousel');
 
@@ -29,7 +38,7 @@ function getDrink(){
       const nextBtn = document.getElementById('nextBtn');
       let currentIndex = 0;
 
-      // Update button visibility based on currentIndex
+    // Update button visibility based on currentIndex
       function updateButtonVisibility() {
         if (currentIndex === 0) {
           prevBtn.style.display = 'block'; // Show "Previous" button at the first item
@@ -43,8 +52,7 @@ function getDrink(){
           nextBtn.style.display = 'block'; // Show "Next" button for other items
         }
       }
-
-      // Initial setup
+    // Initial setup for button visibility
       updateButtonVisibility();
 
       prevBtn.addEventListener('click', () => {
@@ -77,3 +85,50 @@ function getDrink(){
       console.log(`error ${err}`);
     });
 }
+
+function displayDrinkDetails(drink) {
+  // Grab the carousel div and clear the content (delete the carousel)
+  const carousel = document.querySelector('.carousel');
+  const carouselContainer = document.querySelector('.carousel-container')
+  // carouselContainer.classList.add('hide-carousel');
+  // carousel.innerHTML = '';
+  
+
+  // Get the cocktailSelected div and make sure it's empty as well
+  const cocktailSelected = document.querySelector('.cocktailSelected');
+  cocktailSelected.innerHTML = '';
+
+  // Create a template literal to populate the cocktailSelected div with drink details
+  const drinkDetails = `
+    <h2>${drink.strDrink}</h2>
+    <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
+    <span>${drink.strIngredients}</span>
+    <h3>${drink.strInstructions}</h3>
+  `;
+    // Append the drink details to the cocktailSelected div
+  cocktailSelected.innerHTML = drinkDetails;
+}
+
+
+
+      
+        
+// *************************************************************************************
+
+//         drink.strIngredients.forEach((ingred, index) => {
+//           carousel.innerHTML += `
+//             <div class="drinkEntry${index}">
+//               <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
+//               <h2>${drink.strDrink}</h2>
+//             </div>
+//           `;
+
+//             //loop that grabs each drinkEntry result, and then puts data in the HTML by creating elements to put each result into
+//       data.drinks.forEach((drink, index) => {
+//         carousel.innerHTML += `
+//           <div class="drinkEntry${index}">
+//             <img src="${drink.strDrinkThumb}/preview" alt="${drink.strDrink}" />
+//             <h2>${drink.strDrink}</h2>
+//           </div>
+//         `;
+//       });
